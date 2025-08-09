f2ad980b2d02819b0f17b0c61fc4a09df68f28be
cedev <cedevserver@gmail.com>
2025-08-09 14:00:58 +0200

    [add] - nix config

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Jellyfin plugin Discord Notifier";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            dotnetCorePackages.sdk_8_0-bin
+            git
+          ];
+        };
+      });
+}
