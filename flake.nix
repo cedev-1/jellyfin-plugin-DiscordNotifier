36b84dc6f9d9f10bb2eefd4d9ad5043c2c67c9cd
cedev <cedevserver@gmail.com>
2025-08-23 14:57:08 +0200

    [add] - task file + [update] - flake

--- flake.nix
+++ flake.nix
@@ -9,12 +9,20 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { 
+          inherit system;
+          config.allowUnfree = true;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             dotnetCorePackages.sdk_8_0-bin
             git
+            go-task
+
+            # if you dont have docker on your system decomment the following line !
+            # docker
+            # docker-compose
           ];
         };
       });
