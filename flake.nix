bb83db57768c6a0eef2bd582fede6dd0626664f9
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2025-12-16 23:01:57 +0100

    update target version

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
       let
         pkgs = import nixpkgs { 
           inherit system;
@@ -16,7 +16,7 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            dotnetCorePackages.sdk_8_0-bin
+            dotnetCorePackages.sdk_9_0-bin
             git
             go-task
 
